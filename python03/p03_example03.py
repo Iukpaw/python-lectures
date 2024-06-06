2a068ba35c65e9d2e74d72d6b8138c1e5301ad22
Iukpaw <vacatvacat22@gmail.com>
2024-06-06 08:34:33 +0200

    Update p03_example03.py

--- python03/p03_example03.py
+++ python03/p03_example03.py
@@ -1,10 +1,27 @@
-def square_list(numbers):
-    squared_numbers = []
-    for number in numbers:
-        squared_numbers.append(number * number)
-    return squared_numbers
+
+class Employee:
+    def __init__(self, name, department):
+        self.name = name
+        self.department = department
+    
+    def get_info(self):
+        return f"Employee Name: {self.name}, Department: {self.department}"
+
+class Manager(Employee):
+    def __init__(self, name, department, team_size):
+        super().__init__(name, department)
+        self.team_size = team_size
+    
+    def get_info(self):
+        return f"Manager Name: {self.name}, Department: {self.department}, Team Size: {self.team_size}"
 
 
-data = [1, 2, 3, 4]
-squared_data = square_list(data)
-print(squared_data)
+employees = []
+
+employees.append(Employee('John', 'Sales'))
+employees.append(Employee('Kate', 'Marketing'))
+employees.append(Employee('David', 'Sales'))
+employees.append(Manager('George', 'Marketing', 6))
+
+for employee in employees:
+    print(employee.get_info())
